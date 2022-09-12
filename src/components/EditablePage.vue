c36988e1b97bf39fe5e1a6c5c2531bbe264b3e10
RafaelGondi <rdias.ga@gmail.com>
2022-09-12 07:50:18 -0300

    feat: creates tag selection menu

--- src/components/EditablePage.vue
+++ src/components/EditablePage.vue
@@ -7,11 +7,25 @@
 		<contenteditable
 			v-model="content.value"
 			:ref="`el${index}`"
+			:contenteditable="content.editable"
 			:tag="content.tag"
 			:noNl="true"
 			:noHtml="true"
 			@returned="enterPressed(index)"
+			@keypress="slashHandler($event, index)"
 		/>
+	</div>
+	<div
+		v-if="showTagsMenu"
+		class="optionsMenu"
+	>
+		<div
+			v-for="(option, index) in allowedTags"
+			class="opt"
+			@click="changeTag(option)"
+		>
+			<div>{{ option.label }}</div>
+		</div>
 	</div>
 </template>
  
@@ -23,31 +37,65 @@ export default {
 	},
 	data() {
 		return {
-			message: "hello",
-			message2: "",
-			contentTreeLength: 0,
 			contentTree: [
 				{
 					tag: 'p',
-					value: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-					Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-					nisi ut aliquip ex ea commodo consequat.`,
+					value: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
 				},
 				{
 					tag: 'p',
-					value: `Ut enim ad minima veniam, quis nostrum exercitationem ullam
-					corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-					Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-					nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-					nulla pariatur?`,
+					value: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
 				},
 			],
+			contentTreeLength: 2,
+			allowedTags: [
+				{
+					tag: "h1",
+					label: "Heading 1",
+					editable: true,
+				},
+				{
+					tag: "h2",
+					label: "Heading 2",
+					editable: true,
+				},
+				{
+					tag: "h3",
+					label: "Heading 3",
+					editable: true,
+				},
+				{
+					tag: "p",
+					label: "Paragraph",
+					editable: true,
+				},
+				{
+					tag: "i",
+					label: "Italic",
+					editable: true,
+				},
+				{
+					tag: "strong",
+					label: "Bold",
+					editable: true,
+				},
+				{
+					tag: "blockquote",
+					label: "Quote",
+					editable: true,
+				},
+				{
+					tag: "hr",
+					label: "Divider",
+					editable: false,
+				}
+			],
+			tempIndex: 0,
+			showTagsMenu: false,
 		}
 	},
 	methods : {
 		enterPressed(index){
-			console.log('🚀 -> file: EditablePage.vue -> line 50 -> index', index);
 			const newTag = {
 				tag: 'p',
 				value: '',
@@ -60,6 +108,56 @@ export default {
 				this.$refs[`el${index + 1}`][0].$el.focus();
 			});
 		},
+
+		slashHandler(ev, index) {
+			const isTagEmpty = this.$refs[`el${index}`][0].$el.innerText.length < 1;
+
+			if (ev.key !== '/' || !isTagEmpty) {
+				return
+			}
+		
+			this.showTagsMenu = true;
+			this.tempIndex = index;
+		},
+
+		changeTag(option) {
+			this.contentTree[this.tempIndex].tag = option.tag;
+			this.contentTree[this.tempIndex].editable = option.editable;
+
+			this.$nextTick(() => {
+				this.$refs[`el${this.tempIndex}`][0].$el.focus();
+			});
+
+			const newTag = {
+				tag: 'p',
+				value: '',
+			};
+
+			this.contentTree.splice(this.tempIndex + 1, 0, newTag);
+
+			this.showTagsMenu = false;
+		},
 	}
 }
 </script>
+<style>
+	.optionsMenu {
+		box-shadow: rgb(0 0 0 / 5%) 0px 0px 10px, rgb(0 0 0 / 5%) 0px 5px 10px, rgb(0 0 0 / 5%) 0px 2px 4px, rgb(0 0 0 / 5%) 0px 10px 20px;
+		border-radius: 12px;
+		border: 1px solid rgb(231, 234, 240);
+		background-color: rgb(255, 255, 255);
+		padding: 12px;
+		width: fit-content;
+	}
+
+	.opt {
+		margin-bottom: 8px;
+		padding: 4px;
+		border-radius: 4px;
+		cursor: pointer;
+	}
+
+	.opt:hover {
+		background-color: rgb(233, 232, 238);
+	}
+</style>
