4acc15c6d45a964f685fc56c00ad2947ea7a7d7c
RafaelGondi <rdias.ga@gmail.com>
2022-09-11 22:04:33 -0300

    feat: installs vue-contenteditable and begins to use

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,37 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+	<h2>Test</h2>
+	<contenteditable
+		tag="p"
+		v-model="message"
+		:noNl="true"
+		:noHtml="true"
+		@returned="enterPressed"
+	/>
+	<contenteditable
+		tag="p"
+		v-model="message2"
+		:noNl="true"
+		:noHtml="true"
+		@returned="enterPressed"
+	/>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
+ 
+<script>
+import contenteditable from 'vue-contenteditable'; // Not needed it registered globally
+export default {
+	components : {
+		contenteditable
+	},
+	data() {
+		return {
+			message: "hello",
+			message2: ""
+		}
+	},
+	methods : {
+		enterPressed(){
+			console.log('Enter Pressed');
+		}
+	}
 }
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import contenteditable from 'vue-contenteditable'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(contenteditable).mount('#app')
