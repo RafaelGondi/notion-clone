5e6d4167a0f4b642d07fb7d662dd38abff0b4f63
RafaelGondi <rdias.ga@gmail.com>
2022-09-12 14:59:44 -0300

    feat: Implements side menu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import EditablePage from './components/EditablePage.vue'
+import EditablePage from './components/EditablePage.vue';
+import Menu from './components/Menu.vue';
 </script>
 
 <template>
   <EditablePage/>
+  <Menu/>
 </template>
 
 <style scoped>
---
+++ src/components/Menu.vue
@@ -0,0 +1,36 @@
+<template>
+	<div
+		class="side-menu"
+	>
+		<div class="side-menu-header">Notion Clone</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup () {
+		
+
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+	@import 'node_modules/@sysvale/cuida/dist/tokens.scss';
+	.side-menu {
+		height: 100%;
+		width: 300px;
+		background-color: $al-50;
+		position: absolute;
+		top: 0;
+		left: 0;
+		display: flex;
+	}
+
+	.side-menu-header {
+		margin: mTRBL(6, 0, 0, 6);
+		@include heading-3;
+		font-weight: $font-weight-semibold;
+	}
+</style>
