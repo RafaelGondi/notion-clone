a7483d5f0592e6e4887d6e169bf298cbc3fc8340
RafaelGondi <rdias.ga@gmail.com>
2022-09-12 22:02:31 -0300

    refactor: Replaces vue-contenteditable by custom content editable component

--- src/components/EditablePage.vue
+++ src/components/EditablePage.vue
@@ -1,22 +1,5 @@
 <template>
 	<div
-		v-for="(content, index) in fileStore.file.content"
-		:key="index"
-	>
-		<contenteditable
-			v-model="content.value"
-			:placeholder="content.placeholder"
-			:ref="`${fileStore.file.metadata.dom_id}-${index}`"
-			:contenteditable="content.editable"
-			:tag="content.tag"
-			:noNl="true"
-			:noHtml="true"
-			@returned="enterPressed(index)"
-			@keypress="slashHandler($event, index)"
-			@keyup.esc="showTagsMenu = false"
-		/>
-	</div>
-	<div
 		v-if="showTagsMenu"
 		class="optionsMenu"
 	>
@@ -28,18 +11,29 @@
 			<div>{{ option.label }}</div>
 		</div>
 	</div>
+	<div
+		v-for="(content, index) in fileStore.file.content"
+		:key="index"
+	>
+		<component
+			contenteditable="true"
+			:placeholder="content.placeholder"
+			:ref="`${fileStore.file.metadata.dom_id}-${index}`"
+			:is="content.tag"
+			@keyup.enter="enterPressed($event, index)"
+			@keypress="slashHandler($event, index)"
+			@keyup.esc="showTagsMenu = false"
+		>
+			{{ content.value }}
+		</component>
+	</div>
 </template>
  
 <script>
-import contenteditable from 'vue-contenteditable';
 import allowedTags from '../utils/constants/allowedTags';
 import { useFileStore } from '../store/FileStore';
 
 export default {
-	components : {
-		contenteditable
-	},
-
 	setup () {
 		const fileStore = useFileStore();
 
@@ -57,7 +51,9 @@ export default {
 	},
 
 	methods : {
-		enterPressed(index){
+		enterPressed(ev, index){
+			ev.preventDefault();
+
 			const newTag = {
 				tag: 'p',
 				value: '',
@@ -67,12 +63,13 @@ export default {
 			this.fileStore.addContent(newTag, index + 1);
 
 			this.$nextTick(() => {
-				this.$refs[`${this.fileStore.file.metadata.dom_id}-${index + 1}`][0].$el.focus();
+				this.$refs[`${this.fileStore.file.metadata.dom_id}-${index + 1}`][0].focus();
 			});
 		},
 
 		slashHandler(ev, index) {
-			const isTagEmpty = this.$refs[`${this.fileStore.file.metadata.dom_id}-${index}`][0].$el.innerText.length < 1;
+			const isTagEmpty =
+				this.$refs[`${this.fileStore.file.metadata.dom_id}-${index}`][0].innerText.length < 1;
 
 			if (ev.key !== '/' || !isTagEmpty) {
 				return
@@ -98,7 +95,7 @@ export default {
 			this.fileStore.changeTag(tagData, this.tempIndex);
 
 			this.$nextTick(() => {
-				this.$refs[`${this.fileStore.file.metadata.dom_id}-${this.tempIndex}`][0].$el.focus();
+				this.$refs[`${this.fileStore.file.metadata.dom_id}-${this.tempIndex}`][0].focus();
 			});
 
 			this.fileStore.addContent(newTag, this.tempIndex + 1);
@@ -118,7 +115,7 @@ export default {
 		width: fit-content;
 		position: absolute;
 		margin-left: 20px;
-		margin-top: -100px;
+		// margin-top: -50px;
 	}
 
 	.option {
